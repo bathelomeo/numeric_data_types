40a4f1071dc525588c03b5c85e23caa5d836e551
bathelomeo <ngethenathaniel@gmail.com>
2021-09-09 19:24:06 +0400

    Added new files

---
+++ numericdatatypes.py
@@ -0,0 +1,9 @@
+pi = 3.14159
+print(pi)
+
+first_num =  input ('Please enter a number')
+second_num = input ('Please enter a another number')
+print (int(first_num) / int(second_num))
+
+days_in_August = 31
+print(str(days_in_August) + ' ' +'total days in August')
